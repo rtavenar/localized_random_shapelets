c4a310ea4919cdabafe2b410d1b50a8adbb2e595
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2018-10-09 14:35:47 +0200

    Towards PAKDD submission

--- xp_linear_regression.py
+++ xp_linear_regression.py
@@ -2,10 +2,14 @@ from models import ssgl_linear_regression
 
 import numpy
 import matplotlib.pyplot as plt
-import os
+import matplotlib
 
 
-def bracket(ax, pos=[0,0], scalex=1, scaley=1, text="",textkw = {}, linekw = {}):
+def bracket(ax, pos, scalex=1., scaley=1., text="",textkw=None, linekw=None):
+    if textkw is None:
+        textkw = {}
+    if linekw is None:
+        linekw = {}
     x = numpy.array([0, 0.05, 0.45,0.5])
     y = numpy.array([0,-0.01,-0.01,-0.02])
     x = numpy.concatenate((x,x+0.5))
@@ -19,8 +23,8 @@ def bracket(ax, pos=[0,0], scalex=1, scaley=1, text="",textkw = {}, linekw = {})
 font = {'family': 'serif', 'size': 20}
 plt.rc('text', usetex=True)
 plt.rc('font', **font)
-plt.rcParams['text.usetex']=True
-plt.rcParams['text.latex.unicode']=True
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.unicode'] = True
 
 numpy.random.seed(0)
 
@@ -29,29 +33,41 @@ n_features_per_group = 2
 n = 1000
 noise_level = .01
 
-n_features = n_groups * n_features_per_group
+dim = n_groups * n_features_per_group
 groups = numpy.repeat(numpy.arange(n_groups), n_features_per_group)
-ind_sparse = numpy.zeros((n_features, ))
+ind_sparse = numpy.zeros((dim,))
 ind_sparse[2] = 0
 ind_sparse[4] = 0
 
 beta_star = numpy.array([0., 0., 1.5, 2., 0.005, 0.])
 
-X = numpy.random.randn(n, n_features)
+X = numpy.random.randn(n, dim)
 y = numpy.dot(X, beta_star) + noise_level * numpy.random.rand(n)
 
 # Models
-model_ssgl = ssgl_linear_regression(dim_input=n_features, groups=groups, indices_sparse=ind_sparse, alpha=.5, lbda=.01)
+model_ssgl = ssgl_linear_regression(dim_input=dim,
+                                    groups=groups,
+                                    indices_sparse=ind_sparse,
+                                    alpha=.5,
+                                    lbda=.01)
 model_ssgl.fit(X, y.reshape((-1, 1)), epochs=200, verbose=2)
 beta_hat_ssgl = model_ssgl.get_weights()[0].reshape((-1, ))
 mse_ssgl = numpy.linalg.norm(y - model_ssgl.predict(X).reshape((-1, )))
 
-model_lasso = ssgl_linear_regression(dim_input=n_features, groups=None, indices_sparse=numpy.array([1] * n_features), alpha=1., lbda=.01)
+model_lasso = ssgl_linear_regression(dim_input=dim,
+                                     groups=None,
+                                     indices_sparse=numpy.array([1] * dim),
+                                     alpha=1.,
+                                     lbda=.01)
 model_lasso.fit(X, y.reshape((-1, 1)), epochs=200, verbose=2)
 beta_hat_lasso = model_lasso.get_weights()[0].reshape((-1, ))
 mse_lasso = numpy.linalg.norm(y - model_lasso.predict(X).reshape((-1, )))
 
-model_sgl = ssgl_linear_regression(dim_input=n_features, groups=groups, indices_sparse=numpy.array([1] * n_features), alpha=.5, lbda=.01)
+model_sgl = ssgl_linear_regression(dim_input=dim,
+                                   groups=groups,
+                                   indices_sparse=numpy.array([1] * dim),
+                                   alpha=.5,
+                                   lbda=.01)
 model_sgl.fit(X, y.reshape((-1, 1)), epochs=200, verbose=2)
 beta_hat_sgl = model_sgl.get_weights()[0].reshape((-1, ))
 mse_sgl = numpy.linalg.norm(y - model_sgl.predict(X).reshape((-1, )))
@@ -62,22 +78,57 @@ for group_id in range(3):
         labels.append("$\\beta_%d^{(%d)}$" % (feature_id + 1, group_id + 1))
 
 beta_star = numpy.concatenate((beta_star[:2], beta_star[4:], beta_star[2:4]))
-beta_hat_ssgl = numpy.concatenate((beta_hat_ssgl[:2], beta_hat_ssgl[4:], beta_hat_ssgl[2:4]))
-beta_hat_lasso = numpy.concatenate((beta_hat_lasso[:2], beta_hat_lasso[4:], beta_hat_lasso[2:4]))
-beta_hat_sgl = numpy.concatenate((beta_hat_sgl[:2], beta_hat_sgl[4:], beta_hat_sgl[2:4]))
+beta_hat_ssgl = numpy.concatenate((beta_hat_ssgl[:2],
+                                   beta_hat_ssgl[4:],
+                                   beta_hat_ssgl[2:4]))
+beta_hat_lasso = numpy.concatenate((beta_hat_lasso[:2],
+                                    beta_hat_lasso[4:],
+                                    beta_hat_lasso[2:4]))
+beta_hat_sgl = numpy.concatenate((beta_hat_sgl[:2],
+                                  beta_hat_sgl[4:],
+                                  beta_hat_sgl[2:4]))
+
 
 plt.figure(figsize=(12, 4))
-ind = numpy.arange(n_features)
+plt.set_cmap("tab10")
+ind = numpy.arange(dim)
 width = 0.2
-plt.bar(ind, numpy.abs(beta_star), width, label="Ground-truth", lw=2, fc=(0, 0, 0, .3), edgecolor='k')
-plt.bar(ind+width, numpy.abs(beta_hat_ssgl), width, label="SSGL (MSE=%.2f)" % mse_ssgl, lw=2, fc=(0, 0, 1, .3), edgecolor='b')
-plt.bar(ind+2*width, numpy.abs(beta_hat_lasso), width, label="Lasso (MSE=%.2f)" % mse_lasso, lw=2, fc=(0, 1, 0, .3), edgecolor='g')
-plt.bar(ind+3*width, numpy.abs(beta_hat_sgl), width, label="SGL (MSE=%.2f)" % mse_sgl, lw=2, fc=(1, 0, 0, .3), edgecolor='r')
+plt.bar(ind, numpy.abs(beta_star), width,
+        label="Ground-truth",
+        lw=2,
+        alpha=0.6)
+plt.bar(ind+width, numpy.abs(beta_hat_ssgl), width,
+        label="SSGL (MSE=%.2f)" % mse_ssgl,
+        lw=2,
+        alpha=0.6)
+plt.bar(ind+2*width, numpy.abs(beta_hat_lasso), width,
+        label="Lasso (MSE=%.2f)" % mse_lasso,
+        lw=2,
+        alpha=0.6)
+plt.bar(ind+3*width, numpy.abs(beta_hat_sgl), width,
+        label="SGL (MSE=%.2f)" % mse_sgl,
+        lw=2,
+        alpha=0.6)
 plt.gca().set_yscale("log")
 
-bracket(plt.gca(), text="Group 1", pos=[0.05,-.2], scalex=1.5, scaley=2, linekw=dict(lw=1, color="k"))
-bracket(plt.gca(), text="Group 2", pos=[2.05,-.2], scalex=1.5, scaley=2, linekw=dict(lw=1, color="k"))
-bracket(plt.gca(), text="Group 3", pos=[4.05,-.2], scalex=1.5, scaley=2, linekw=dict(lw=1, color="k"))
+bracket(plt.gca(),
+        text="Group 1",
+        pos=[0.05,-.2],
+        scalex=1.5,
+        scaley=2,
+        linekw=dict(lw=1, color="k"))
+bracket(plt.gca(),
+        text="Group 2",
+        pos=[2.05,-.2],
+        scalex=1.5,
+        scaley=2,
+        linekw=dict(lw=1, color="k"))
+bracket(plt.gca(),
+        text="Group 3",
+        pos=[4.05,-.2],
+        scalex=1.5,
+        scaley=2,
+        linekw=dict(lw=1, color="k"))
 
 plt.legend(loc="upper left")
 plt.ylabel("Beta values")
